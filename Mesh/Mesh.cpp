d32d4e7dd0dbcede188bd836e9456c1d6996260a
Kevin J. Dugan <dugankj@ornl.gov>
2019-06-14 20:07:40 -0400

    Refactoring initialization

--- Mesh/Mesh.cpp
+++ Mesh/Mesh.cpp
@@ -13,6 +13,15 @@ Mesh::Mesh(const std::vector<float> &minVerts,
     assert (this->dimension >= 1 and this->dimension <= 3);
     this->owningProcessor = processor;
 
+    this->generateVertices(minVerts, maxVerts, nCells);
+    this->generateElements(nCells);
+
+}
+
+void Mesh::generateVertices(const std::vector<float> &minVerts,
+                            const std::vector<float> &maxVerts,
+                            const std::vector<unsigned int> &nCells){
+
     // Get delta for each direction
     std::vector<float> delta;
     for (unsigned int i = 0; i < this->dimension; i++)
@@ -22,18 +31,18 @@ Mesh::Mesh(const std::vector<float> &minVerts,
     unsigned int nVertices = 1;
     for (const auto d : nCells)
         nVertices *= (d+1);
-    std::vector<std::vector<float>> vertices(nVertices, std::vector<float>(this->dimension, 0.0));
+    std::vector<std::vector<float>> vertexCoordinates(nVertices, std::vector<float>(this->dimension, 0.0));
     for (unsigned int dim = 0; dim < this->dimension; dim++){
         unsigned int repeat = 1;
         for (unsigned int d = 0; d < dim; d++)
             repeat *= (nCells[d] + 1);
 
         unsigned int index = 0;
-        while (index < vertices.size()){
+        while (index < vertexCoordinates.size()){
             for (unsigned int point = 0; point <= nCells[dim]; point++){
                 float value = point * delta[dim];
                 for (unsigned int r = 0; r < repeat; r++){
-                    vertices[index][dim] = value;
+                    vertexCoordinates[index][dim] = value;
                     index++;
                 }
             }
@@ -41,8 +50,8 @@ Mesh::Mesh(const std::vector<float> &minVerts,
     }
 
     // Create Node List
-    this->vertices.reserve(vertices.size());
-    for (const auto v : vertices)
+    this->vertices.reserve(vertexCoordinates.size());
+    for (const auto v : vertexCoordinates)
         this->vertices.push_back( std::make_shared<Node>(Node(this->dimension, v)) );
 
     // for (const auto &v : vertices)
@@ -50,6 +59,10 @@ Mesh::Mesh(const std::vector<float> &minVerts,
     //         printf("(%4.1f, %4.1f)\n", v[0], v[1]);
     //     else if (v.size() == 3)
     //         printf("(%4.1f, %4.1f, %4.1f)\n", v[0], v[1], v[2]);
+}
+
+void Mesh::generateElements(const std::vector<unsigned int> &nCells){
+    assert (this->vertices.size() > 0);
 
     // Setup Element Connectivity
     unsigned int nElements = 1;
@@ -109,7 +122,6 @@ Mesh::Mesh(const std::vector<float> &minVerts,
     //         printf("[%4d, %4d, %4d, %4d]\n", v[0], v[1], v[2], v[3]);
     //     else if (v.size() == 8)
     //         printf("[%4d, %4d, %4d, %4d, %4d, %4d, %4d, %4d]\n", v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
-
 }
 
 void Mesh::print(std::ostream &out) const {
--- Mesh/Mesh.h
+++ Mesh/Mesh.h
@@ -21,7 +21,10 @@ class Mesh {
         std::vector<std::shared_ptr<Element>> elements;
         std::vector<std::vector<unsigned int>> elementConnectivity;
 
-        // void generateVertices()
+        void generateVertices(const std::vector<float> &minVerts,
+                              const std::vector<float> &maxVerts,
+                              const std::vector<unsigned int> &nCells);
+        void generateElements(const std::vector<unsigned int> &nCells);
 };
 
 #endif // MESH_H_C0A0
